8b9d0899079bf93ce5229b2b36b292b3b0af75a1
lisp3r <lisp3r@github.com>
2024-06-12 17:41:40 +0400

    main.py: - Added Config class instead of separate functions - Added config validation function outside of Config class to make it possible to use "config set" command to fix problems - Changed banner() -> string to print it with argparse help

--- src/main.py
+++ src/main.py
@@ -19,40 +19,92 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 data_file = os.path.join(dir_path, "machine_data.json")
 
 
-def load_config():
-    with open(os.path.join(dir_path, 'config.json'), 'r') as config_file:
-        return json.load(config_file)
+class Config:
+    def __init__(self, config_path: str):
+        self.config_path = config_path
+        self.config = {}
+        self.load()
 
+    def save(func):
+        def inner(*args, **kwargs):
+            result = func(*args, **kwargs)
+            self = args[0]
 
-def save_config(config_data):
-    with open(os.path.join(dir_path, 'config.json'), 'w') as config_file:
-        return json.dump(config_data, config_file)
+            with open(self.config_path, 'w') as config_file:
+                json.dump(self.config, config_file, indent=4)
 
+            return result
+        return inner
 
-# Source: https://stackoverflow.com/a/57561744
-def set_var_config(key_string, value):
-    """Given 'platforms.htb', 'path/to/folder', set config_data['platforms']['htb'] = 'path/to/folder'"""
+    def __load(self):
+        try:
+            with open(self.config_path, 'r') as config_file:
+                self.config = json.load(config_file)
+        except json.decoder.JSONDecodeError as err:
+            console.print(f'[red][!] Cannot parse config file: {err}')
+            exit(1)
 
-    config_data = load_config()
-    keys = key_string.split(".")
+    def load(self):
+        self.__load()
+        self.__repair()
 
-    # For every key *before* the last one, we concentrate on navigating through the dictionary.
-    for key in keys[:-1]:
-        # Try to find config_data[key]. If it doesn't exist, create it with an empty dictionary. Then,
-        # update our `config_data` pointer to refer to the thing we just found (or created).
-        config_data = config_data.setdefault(key, {})
+    # TODO: it is kinda ugly so maybe rewrite it later
+    @save
+    def __repair(self):
+        """ Repairs broken config file.
+        First of all, we want to have 'platforms' nested dict being not empty. The function will create it and fill
+        with default data.
+        :return: None
+        """
+        if not self.config.get('platforms'):
+            self.config['platforms'] = {
+                'htb': '/home/user/htb',
+                'thm': '/home/user/thm'
+            }
 
-    # Finally, set the final key to the given value
-    config_data[keys[-1]] = value
-    save_config(config_data)
+    @save
+    def rm(self, key_string: str):
+        """ Removes key from config
+        Example:
+            self.config = {'platforms': { 'htb': 'path/to/htb' }, 'user': { 'name': 'alex' }}
+
+            1. key_string: 'platforms.htb'
+               result: {'platforms': {}, 'user': {'name': 'alex'}}
+            2. key_string: 'user'
+               result: {'platforms': { 'htb': 'path/to/htb' }}
+
+        :param key_string: key or list of keys joined with '.', e.g. 'platforms.htb'
+        :return: None
+        """
+        here = self.config
+        keys = key_string.split('.')
+        for key in keys[:-1]:
+            here = here.setdefault(key, {})
+        del here[keys[-1]]
+
+    @save
+    def set(self, key_string: str, value: str):
+        """ Adds value to config
+        Example:
+            self.config = {}
+
+            key_string: 'platforms.htb', value: 'path/to/htb'
+            result: {'platforms': {'htb': 'path/to/htb'}}
+
+        :param key_string: key or list of keys joined with '.', e.g. 'platforms.htb'
+        :param value: value
+        :return: None
+        """
+        here = self.config
+        keys = key_string.split('.')
+        for key in keys[:-1]:
+            here = here.setdefault(key, {})
+        here[keys[-1]] = value
 
 
 console = Console()
-config_data = load_config()
 
-
-def banner():
-    console.print("""
+BANNER = """
 [cyan]
  ▄▄▄·  ▄· ▄▌      
 ▐█ ▀█ ▐█▪██▌ ▄█▀▄ 
@@ -61,7 +113,7 @@ def banner():
  ▀  ▀   ▀ •  ▀█▄▀▪
 
 [/]                                              
-""")
+"""
 
 
 def get_box_data():
@@ -85,20 +137,52 @@ def print_box_info():
     table.add_column("PLATFORM", style="blue") 
     table.add_column("STATUS", style="blue") 
 
-    # banner()
     table.add_row(current_box, rhost, domain, platform, status)
     console.print(table) 
 
 
+def update_config(args, config, print_help):
+    if args.set:
+        path, var = args.set
+        config.set(path, var)
+    elif args.print:
+        print(json.dumps(config.config, indent=4))
+    elif args.rm:
+        config.rm(args.rm)
+    else:
+        print_help()
+    exit(0)
+
+
+def config_validation(config: Config):
+    """ Validate config data: 1) make user to solve errors found on config loading; 2) check paths to project dirs
+
+    :param config: Config object
+    :return: None
+    """
+    wrong_dirs = []
+    for platform_dir_name, platform_dir_path in config.config['platforms'].items():
+        if not os.path.exists(platform_dir_path):
+            wrong_dirs.append((platform_dir_name, platform_dir_path))
+    if wrong_dirs:
+        console.print(f'Set the correct paths for project folders.\n\nCurrent valuables are:')
+        for i in wrong_dirs:
+            console.print(f'\t{i[0]}: {i[1]}')
+        console.print(f'\nTo change them use "config" command:\n\tayo config set platforms.{wrong_dirs[0][0]} <value>')
+        exit(0)
+
+
 def main():
-    parser = argparse.ArgumentParser(description='AYO - CTF Manager [Help Menu]')
+    config_data = Config(os.path.join(dir_path, '../config.json'))
+
+    parser = argparse.ArgumentParser(description=f'{BANNER}AYO - CTF Manager [Help Menu]')
     subparsers = parser.add_subparsers(dest='command', help='Update or get data')
 
     parser_new = subparsers.add_parser('new', help='Add new CTF box')
     parser_new.add_argument('--box', '-b', required=True, type=str, dest='ctf_name', help='Name of the CTF Box')
     parser_new.add_argument('--rhost', '-r', required=True, type=str, help='$rhost IP address')
     parser_new.add_argument('--domain', '-d', required=True, type=str, help='Domain Name')
-    parser_new.add_argument('--platform', '-p', required=True, type=str, choices=config_data['platforms'].keys(), help='Platform Name')
+    parser_new.add_argument('--platform', '-p', required=True, type=str, choices=config_data.config.get('platforms', {}).keys(), help='Platform Name')
     parser_new.add_argument('--active', '-a', default='active', choices=['active', 'dead'], help='Status of the CTF (active/dead)')
 
     parser_get = subparsers.add_parser('get', help='Get CTF box info')
@@ -110,40 +194,22 @@ def main():
 
     parser_conf = subparsers.add_parser('config', help='Configure AYO')
     parser_conf.add_argument('--set', type=str, nargs=2, help='Set a variable into config')
+    parser_conf.add_argument('--rm', type=str, help='Remove a variable from config')
     parser_conf.add_argument('--print', action="store_true", help='Print config')
+
+    COMMANDS_TO_SKIP_CONFIG_VALIDATION = ['config']
 
     args = parser.parse_args()
 
     if args.command is None:
-        banner()
         parser.print_help()
         exit(0)
 
+    if args.command not in COMMANDS_TO_SKIP_CONFIG_VALIDATION:
+        config_validation(config_data)
+
     if args.command == 'config':
-        if args.set:
-            path, var = args.set
-            set_var_config(path, var)
-            exit(0)
-        if args.print:
-            print(json.dumps(config_data, indent=4))
-            exit(0)
-
-    # Check if the user changed htm and thm paths
-    if not (os.path.exists(config_data['platforms']['thm']) and
-            os.path.exists(config_data['platforms']['htb'])):
-        console.print(f"""First of all, set the correct paths for 'HTB' and 'THM folders.'
-Current valuables are:
-    htb_path: {config_data['platforms']['htb']}
-    thm_path: {config_data['platforms']['thm']}
-
-To change them use:
-    ayo config --set platforms.htb <value>
-    ayo config --set platforms.thm <value>
-""")
-        exit(0)
-
-    banner()
-
+        update_config(args, config_data, parser_conf.print_help)
     if args.command == 'new':
         new_box(args)
     if args.command == 'get':
